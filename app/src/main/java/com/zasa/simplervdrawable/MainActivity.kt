da99d5b1727b917e08e50ca17fcc61c83d8e9c52
zansangeeth <zansangeeth17@gmail.com>
2022-08-17 12:39:25 +0530

    implemented fully updated simple recycler with local images

--- app/src/main/java/com/zasa/simplervdrawable/MainActivity.kt
+++ app/src/main/java/com/zasa/simplervdrawable/MainActivity.kt
@@ -2,10 +2,58 @@ package com.zasa.simplervdrawable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvNews : RecyclerView
+    private lateinit var newsList : ArrayList<News>
+    private lateinit var imageId : Array<Int>
+    private lateinit var heading : Array<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageId = arrayOf(
+            R.drawable.a,
+            R.drawable.b,
+            R.drawable.c,
+            R.drawable.d,
+            R.drawable.e,
+            R.drawable.f,
+            R.drawable.g,
+            R.drawable.h,
+            R.drawable.i,
+        )
+
+        heading = arrayOf(
+            "Biden aims to expand vaccines for adults and children",
+            "Just got my first shot, helping the world to be a safer place",
+            "Local trains to be suspended in Bengal from tomorrow in view of covid-19",
+            "MHA asks states,UTs to ensure there are no fires in hospitals",
+            "Australian citizen sues PM Morrison over flight ban from india",
+            "Former India hockey coach Kaushik hospitalised after testing positive for COVID",
+            "Delhi records 20,960 fresh covid-19 infections, positivity rate at 26.37%",
+            "Barcelona church offers open-air space for Ramadan",
+            "Trillions of cicadas set to emerge in the US, here's why",
+            "Homemaker, economist: Candidates from all walks of life in Bengal assembly"
+        )
+
+        rvNews = findViewById(R.id.rvNews)
+        rvNews.layoutManager = LinearLayoutManager(this)
+        newsList = arrayListOf<News>()
+
+        getUserData()
+
+    }
+
+    private fun getUserData() {
+        for (i in imageId.indices){
+            val news = News(imageId[i], heading[i])
+            newsList.add(news)
+        }
+        rvNews.adapter = NewsAdapter(newsList)
     }
 }
