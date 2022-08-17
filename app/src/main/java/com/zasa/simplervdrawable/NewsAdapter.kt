3a54182a559f88e86c18bc29e574020d74aa3134
zansangeeth <zansangeeth17@gmail.com>
2022-08-17 12:21:13 +0530

    implemented custom adapter

--- app/src/main/java/com/zasa/simplervdrawable/NewsAdapter.kt
+++ app/src/main/java/com/zasa/simplervdrawable/NewsAdapter.kt
@@ -1,6 +1,10 @@
 package com.zasa.simplervdrawable
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -8,22 +12,23 @@ import androidx.recyclerview.widget.RecyclerView
  **@Author -> Sangeeth on 8/17/2022
  */
 class NewsAdapter(private val newsList : ArrayList<News>) : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
-    
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_news, parent, false)
+        return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = newsList[position]
+        holder.titleImage.setImageResource(currentItem.titleImage)
+        holder.heading.text = currentItem.heading
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    override fun getItemCount() = newsList.size
+
+    inner class ViewHolder(itemView :View) : RecyclerView.ViewHolder(itemView) {
+        val titleImage : ImageView = itemView.findViewById(R.id.ivImage)
+        val heading : TextView = itemView.findViewById(R.id.tvHeading)
     }
-
-    inner class ViewHolder {
-
-    }
-
 
 }
